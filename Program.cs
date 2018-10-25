00811b50c57ac679631b2491f32554a82ae831c1
William Li <wul@microsoft.com>
2018-10-25 13:09:22 -0700

    try to repro

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace appsettingrepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+	        .SetBasePath(Directory.GetCurrentDirectory())
+	        .AddJsonFile("appsettings.json")
+            .Build();
+
+            var dict = new Dictionary<string, string>();
+            config.GetSection("section0").Bind(dict);
+            Console.WriteLine(dict["key0"]);
+        }
+    }
+}
